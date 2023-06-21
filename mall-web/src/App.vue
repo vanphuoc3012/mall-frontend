006cdc4733f9b3b81d501e878abcd87ee95a8344
vanphuoc <vanphuoc3012@gmail.com>
2023-06-21 16:50:32 +0700

    git

---
+++ mall-web/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+</style>
